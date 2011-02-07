4b37297857416d0f32edee76cde4c2209c99e961
Lin Jen-Shin <godfat@godfat.org>
2011-02-08 06:09:18 +0800

    cplusplus/temp.cpp: copied from ~/test/temp.cpp

---
+++ cplusplus/temp.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <list>
+using namespace std;
+
+#line 1 "definitions"
+int main(int argc, char* argv[])
+{
+
+
+#line 1 "main"
+int i;
+int g = 10;
+vector<int> v;
+v.push_back(g);
+copy(v.begin(), v.end(), ostream_iterator<int>(cout, "\n"));
+exit(0);
+exit(0);
+1;
+2;
+cout.flush();
+cout << 1;
+
+#line 1 "query"
+
+}
+
