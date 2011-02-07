0b0a53727be474b79daf3f1db2f456287f7bbf57
Lin Jen-Shin <godfat@godfat.org>
2011-02-08 05:33:44 +0800

    python: copied from ~/project/test/*.py

---
+++ python/lib.py
@@ -0,0 +1,12 @@
+# lib.py
+class Lib:
+  def greet():
+    print('hi')
+
+def main():
+  print('my name is: ' + __name__)
+
+if __name__ == '__main__':
+  main()
+
+main()
---
+++ python/my.py
@@ -0,0 +1,4 @@
+# my.py
+import lib
+
+lib.Lib.greet()
