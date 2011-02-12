bef13e4de510c28f41f1662cd855c552239655f3
Lin Jen-Shin <godfat@godfat.org>
2011-02-13 02:32:13 +0800

    parser.rb: hand written reader

--- ruby/parser.rb
+++ ruby/parser.rb
@@ -1,3 +1,32 @@
+# encoding: utf-8
+
+class Reader
+  def initialize sock
+    if sock.respond_to?(:read)
+      self.sock   = sock
+      self.buffer = ''
+    else
+      self.buffer = sock.to_str
+    end
+  end
+
+  def peek
+    buffer && buffer[0]
+  end
+
+  def getc
+    buffer && if buffer.empty?
+                raise EOFError.new if sock.nil? || sock.eof?
+                self.buffer = sock.read(1024)
+                getc
+              else
+                buffer.slice!(0)
+              end
+  end
+
+  private
+  attr_accessor :sock, :buffer
+end
 
 class Parser
   Expression = %r{
