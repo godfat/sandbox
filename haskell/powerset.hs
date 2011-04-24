9e6235f048826d2d075af090a42d305b78945d33
Lin Jen-Shin <godfat@godfat.org>
2011-04-24 19:53:59 +0800

    powerset.hs: add some more implementations

--- haskell/powerset.hs
+++ haskell/powerset.hs
@@ -1,8 +1,27 @@
 
-module Powerset where
+import Control.Monad (filterM)
 
 powerset :: [a] -> [[a]]
 powerset     [] = [[]]
 powerset (x:xs) = concat $ map (powerset') [True, False] where
   -- powerset' :: Bool -> [[a]]
   powerset' flg = map (\ys -> if flg then x:ys else ys) (powerset xs)
+
+powerset' :: [a] -> [[a]]
+powerset'     [] = [[]]
+powerset' (x:xs) = map (x:) (powerset xs) ++ (powerset xs)
+--                 True part
+--                                           False part
+
+powerset'' :: [a] -> [[a]]
+powerset'' = filterM (const [True, False])
+
+powerset''' :: [a] -> [[a]]
+powerset'''     [] =  [[]]
+powerset''' (x:xs) = do
+  flag <- [True, False]
+  ys   <- powerset''' xs
+  if flag then
+    return (x:ys)
+  else
+    return ys
