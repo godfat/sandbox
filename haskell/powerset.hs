e9009558207229b1d9856027375de06865a54a43
Lin Jen-Shin <godfat@godfat.org>
2011-04-25 23:26:57 +0800

    powerset.hs: rearrange definition order

--- haskell/powerset.hs
+++ haskell/powerset.hs
@@ -1,23 +1,29 @@
 
 import Control.Monad (filterM)
 
---         1.65 real         0.08 user         0.01 sys
+--         1.16 real         0.05 user         0.01 sys
 powerset :: [a] -> [[a]]
-powerset     [] = [[]]
-powerset (x:xs) = concat $ map (powerset') [True, False] where
-  -- powerset' :: Bool -> [[a]]
-  powerset' flg = map (\ys -> if flg then x:ys else ys) (powerset xs)
+powerset = filterM (const [True, False])
 
---         1.64 real         0.09 user         0.01 sys
+
+
+--         1.65 real         0.08 user         0.01 sys
 powerset' :: [a] -> [[a]]
 powerset'     [] = [[]]
-powerset' (x:xs) = map (x:) (powerset xs) ++ (powerset xs)
+powerset' (x:xs) = concat $ map (powerset_) [True, False] where
+  -- powerset' :: Bool -> [[a]]
+  powerset_ flg = map (\ys -> if flg then x:ys else ys) (powerset' xs)
+
+
+
+--         1.64 real         0.09 user         0.01 sys
+powerset'' :: [a] -> [[a]]
+powerset''     [] = [[]]
+powerset'' (x:xs) = map (x:) (powerset'' xs) ++ (powerset'' xs)
 --                 True part
 --                                           False part
 
---         1.16 real         0.05 user         0.01 sys
-powerset'' :: [a] -> [[a]]
-powerset'' = filterM (const [True, False])
+
 
 --         1.53 real         0.08 user         0.01 sys
 powerset''' :: [a] -> [[a]]
@@ -30,6 +36,8 @@ powerset''' (x:xs) = do
   else
     return ys
 
+
+
 --         1.44 real         0.06 user         0.01 sys
 powerset'''' :: [a] -> [[a]]
 powerset'''' = (powerset''''rec (:[])) . reverse
@@ -37,5 +45,7 @@ powerset'''' = (powerset''''rec (:[])) . reverse
 powerset''''rec :: ([a] -> [[a]]) -> [a] -> [[a]]
 powerset''''rec k     [] = k []
 powerset''''rec k (x:xs) = powerset''''rec (\r -> (map (x:) (k r)) ++ k r) xs
+
+
 
 main = putStrLn $ show $ powerset'''' [1..15]
