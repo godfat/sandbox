584709a990eafac36d2c404582ae88c83fce7ec5
Lin Jen-Shin <godfat@godfat.org>
2011-04-25 23:19:29 +0800

    collatz.hs: how to print line by line note

--- haskell/collatz.hs
+++ haskell/collatz.hs
@@ -8,7 +8,15 @@ collatz' step n | n == 1 = step
                 |  odd n = collatz' (step+1) (n * 3 + 1)
 
 result = do
-  i <- [1..10000]
+  i <- [1..50000]
   "L[" ++ show i ++ "] = " ++ show (collatz i) ++ "\n"
 
 main = putStr result
+
+--result [] [] = return ()
+--result (i:is) (x:xs) = do
+--  putStrLn $ "L[" ++ show i ++ "] = " ++ show x
+--  result is xs
+
+--main = do
+--  result input (map collatz input) where input = [1..50000]
