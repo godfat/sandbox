64e59f33db6a5ff42ce92c10e7557d91cdc9b140
Lin Jen-Shin <godfat@godfat.org>
2011-06-17 22:27:07 +0800

    godfat-quine.rb

---
+++ ruby/godfat-quine.rb
@@ -0,0 +1,11 @@
+# _="_=%p;puts _%%_";puts _%_
+
+# "Lin Jen Shin (godfat)%p;puts((ObjectSpace.each_object.find{|s|s.to_s.size==140}%%ObjectSpace.each_object.find{|s|s.to_s.size==140})[21..-1])";puts((ObjectSpace.each_object.find{|s|s.to_s.size==140}%ObjectSpace.each_object.find{|s|s.to_s.size==140})[21..-1])
+
+# GC.disable # for rubinius
+
+# "Lin Jen Shin (godfat) is a programmer who works for Cardinal Blue Software, loving computer games, Haskell, open source, and self-referential jokes, such as: %p;puts((ObjectSpace.each_object.find{|s|s.to_s.size==278}%%ObjectSpace.each_object.find{|s|s.to_s.size==278})[158..-1])";puts((ObjectSpace.each_object.find{|s|s.is_a?(String)&&s.size==278}%ObjectSpace.each_object.find{|s|s.is_a?(String)&&s.size==278})[158..-1])
+
+# "Lin Jen Shin (godfat) is a programmer who works for Cardinal Blue Software, loving computer games, Haskell, open source, and self-referential jokes, such as: %p=~/(.+)/;puts(($1%%$1)[158..-1])"=~/(.+)/;puts(($1%$1)[158..-1])
+
+"Lin Jen Shin (godfat) is a programmer who works for Cardinal Blue Software, loving computer games, Haskell, open source, and self-referential jokes, such as: %p=~/(.+)/;puts(($1%%$1).sub(/^[^\"]+/,''))"=~/(.+)/;puts(($1%$1).sub(/^[^"]+/,''))
