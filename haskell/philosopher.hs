f1aeb05038461a9ca5a9957f719d7b566ceb99dd
Lin Jen-Shin <godfat@godfat.org>
2011-06-22 04:29:34 +0800

    philosopher.hs: added a random delay so that it shows diff result

--- haskell/philosopher.hs
+++ haskell/philosopher.hs
@@ -1,11 +1,10 @@
 
 -- http://computationalthoughts.blogspot.com/2008/03/some-examples-of-software-transactional.html
 
-import System.Random (newStdGen, randomR)
-import System.Posix (sleep)
+import System.Random (newStdGen, randomR, StdGen)
 
 import Control.Monad (replicateM)
-import Control.Concurrent (forkIO)
+import Control.Concurrent (forkIO, threadDelay)
 import Control.Concurrent.STM (atomically, retry, TVar, STM,
                                newTVar, readTVar, writeTVar)
 
@@ -15,11 +14,12 @@ type TFork = TVar Fork
 -- WriterT [String] IO Int
 
 main = do
-  gen   <- newStdGen
+  gen   <- newStdGen >>= \g -> atomically (newTVar g)
   forks <- atomically forksSTM
   buffer <- atomically (newTVar [])
   for [0..4] `at` \i ->
-    forkIO `at` philosopher i buffer (forks !! i) (forks !! ((i + 1) `mod` 5))
+    forkIO `at` philosopher i buffer gen
+                  (forks !! i) (forks !! ((i + 1) `mod` 5))
   monitor buffer
 
 forksSTM :: STM [TFork]
@@ -28,8 +28,16 @@ forksSTM = replicateM 5 newForkSTM
 newForkSTM :: STM TFork
 newForkSTM = newTVar Free
 
-philosopher :: Int -> TVar [String] -> TFork -> TFork -> IO ()
-philosopher n buffer fork0 fork1 = do
+philosopher :: Int -> TVar [String] -> TVar StdGen -> TFork -> TFork -> IO ()
+philosopher n buffer tgen fork0 fork1 = do
+  delay <- atomically `at` do
+             gen <- readTVar tgen
+             let (delay, gen') = randomR (1, 1000) gen
+             writeTVar tgen gen'
+             return delay
+
+  threadDelay delay
+
   atomically `at` do
     tell buffer ("Philosopher " ++ show n ++ " is eating.")
 
@@ -44,7 +52,7 @@ philosopher n buffer fork0 fork1 = do
     think fork0
     think fork1
 
-  philosopher n buffer fork0 fork1
+  philosopher n buffer tgen fork0 fork1
 
 --
 
