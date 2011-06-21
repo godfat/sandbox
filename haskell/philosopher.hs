d78fba8ab1f4e37329f7948a7667fbde472e3f5a
Lin Jen-Shin <godfat@godfat.org>
2011-06-22 03:38:27 +0800

    philosopher.hs: the true first working philosopher without logging

--- haskell/philosopher.hs
+++ haskell/philosopher.hs
@@ -17,10 +17,8 @@ type TFork = TVar Fork
 main = do
   gen   <- newStdGen
   forks <- atomically forksSTM
-  buffer <- atomically (newTVar [])
   for [0..4] `at` \i ->
-    forkIO `at` philosopher i buffer (forks !! i) (forks !! ((i + 1) `mod` 5))
-  monitor buffer
+    forkIO `at` philosopher (forks !! i) (forks !! ((i + 1) `mod` 5))
 
 forksSTM :: STM [TFork]
 forksSTM = replicateM 5 newForkSTM
@@ -28,45 +26,17 @@ forksSTM = replicateM 5 newForkSTM
 newForkSTM :: STM TFork
 newForkSTM = newTVar Free
 
-philosopher :: Int -> TVar [String] -> TFork -> TFork -> IO ()
-philosopher n buffer fork0 fork1 = do
-  atomically `at` do
-    tell buffer ("Philosopher " ++ show n ++ " is eating.")
-
+philosopher :: TFork -> TFork -> IO ()
+philosopher fork0 fork1 = do
   atomically `at` do
     eat   fork0
     eat   fork1
 
   atomically `at` do
-    tell buffer ("Philosopher " ++ show n ++ " is thinking.")
-
-  atomically `at` do
     think fork0
     think fork1
 
-  philosopher n buffer fork0 fork1
-
---
-
-monitor :: TVar [String] -> IO ()
-monitor buffer = do
-  sentences <- atomically (flush buffer)
-  for sentences putStrLn
-  monitor buffer
-
-tell :: TVar [String] -> String -> STM ()
-tell var sentence = do
-  buffer <- readTVar var
-  writeTVar var (buffer ++ [sentence])
-
-flush :: TVar [String] -> STM [String]
-flush var = do
-  buffer <- readTVar var
-  case buffer of
-    [] -> retry
-    _  -> do
-      writeTVar var []
-      return buffer
+  philosopher fork0 fork1
 
 --
 
