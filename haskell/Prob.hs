df91f1cebc4fa2d9479cf7efd05179bf7c96c10b
Lin Jen-Shin <godfat@godfat.org>
2011-07-03 21:27:54 +0800

    Probability monad/functor, along with quickcheck for functor law

---
+++ haskell/Prob.hs
@@ -0,0 +1,56 @@
+
+module Prob (Prob, join) where
+
+import Data.Ratio (Rational, (%))
+import Control.Applicative (Applicative, pure, (<$>), (<*>))
+
+import Test.QuickCheck (quickCheck)
+import Test.QuickCheck.Arbitrary (Arbitrary, arbitrary)
+import Test.QuickCheck.Function -- why can't i import only for Fun?
+
+newtype Prob a = Prob { getProb :: [(a, Rational)] } deriving (Show, Eq)
+
+instance Functor Prob where
+  fmap f (Prob prob) = Prob $ map (\(x, p) -> (f x, p)) prob
+
+instance Applicative Prob where
+  pure a = Prob [(a, 1)]
+  apps <*> prob = do
+    f <- apps
+    p <- prob
+    return (f p)
+
+instance Monad Prob where
+  return = pure
+  p >>= f = join (fmap f p)
+
+join :: Prob (Prob a) -> Prob a
+join (Prob prob) = Prob $ concat $ map
+  (\((Prob prob'), p) -> map (\(x, p') -> (x, p * p')) prob') prob
+
+----------------------------------------------------------------------------
+
+coin :: Prob Bool
+coin = Prob [(True, 1%2), (False, 1%2)]
+
+----------------------------------------------------------------------------
+
+functorLawId :: Eq a => Prob a -> Bool
+functorLawId prob = fmap id prob == id prob
+
+functorLawComposition :: Eq c => (Fun b c) -> (Fun a b) -> Prob a -> Bool
+functorLawComposition (Fun _ f) (Fun _ g) prob =
+  fmap (f . g) prob == (fmap f . fmap g) prob
+
+----------------------------------------------------------------------------
+
+instance Arbitrary a => Arbitrary (Prob a) where
+  arbitrary = fmap Prob arbitrary
+
+----------------------------------------------------------------------------
+
+main = do
+  quickCheck (functorLawId :: Prob  Int -> Bool)
+  quickCheck (functorLawId :: Prob Bool -> Bool)
+  quickCheck (functorLawComposition ::
+                (Fun Char Int) -> (Fun Bool Char) -> Prob Bool -> Bool)
