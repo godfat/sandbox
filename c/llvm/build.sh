ca87e07274cbc389ef2a852174521a2c8b3caa67
Lin Jen-Shin <godfat@godfat.org>
2011-11-19 00:14:51 +0800

    c/llvm: a quick llvm assembly/bitcode conversion demo

---
+++ c/llvm/build.sh
@@ -0,0 +1,11 @@
+clang -emit-llvm main.c -c -o main.bc
+lli main.bc
+llvm-dis main.bc -o main.ll
+lli main.ll
+llvm-as main.ll -o main.bc
+lli main.bc
+llc main.bc -o main.s
+as main.s -o main.o
+ld /usr/lib/libc.dylib /usr/lib/crt1.o main.o -o main
+# ld /usr/lib/libc.so /usr/lib/crt1.o /usr/lib/crti.o /usr/lib/crtn.o /usr/lib/libc_nonshared.a main.o -o main
+./main
