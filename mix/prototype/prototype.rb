efbe6fbc6a560fae51b15f876630ca9d7eff307a
caasi Huang <caasi.igd@gmail.com>
2012-02-26 05:27:41 +0800

    JavaScript prototype work like this.

---
+++ mix/prototype/prototype.rb
@@ -0,0 +1,32 @@
+class Prototype
+  attr_accessor :prototype
+  def initialize *obj
+    if(obj.length != 0)
+      raise "not a prototype instance!" unless obj.first.respond_to? "prototype"
+      @__proto__ = obj.first.prototype
+    end
+  end
+  def method_missing msg, *args, &block
+    @__proto__.send msg, *args, &block
+  end
+end
+
+Cat = Prototype.new
+def Cat.meow
+  puts "meow~"
+end
+
+# set Alice's prototype to Cat
+Alice = Prototype.new
+Alice.prototype = Cat
+# but Alice can't meow yet
+#Alice.meow
+
+a = Prototype.new Alice
+a.meow
+
+def Cat.jump
+  puts "**jump**"
+end
+
+a.jump
