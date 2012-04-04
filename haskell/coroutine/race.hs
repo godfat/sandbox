209eeb7314d0a18f6f9de52c2da4825e0eb4e10a
Lin Jen-Shin <godfat@godfat.org>
2012-04-04 22:51:05 +0800

    haskell/coroutine/race.hs: demonstrate race conditions in coroutine

---
+++ haskell/coroutine/race.hs
@@ -0,0 +1,25 @@
+
+-- cabal install monad-coroutine
+
+import Data.IORef (IORef, newIORef, readIORef, writeIORef)
+import Control.Monad.Trans (lift)
+import Control.Monad.Coroutine (Coroutine, resume)
+import Control.Monad.Coroutine.SuspensionFunctors (Yield(Yield), yield)
+
+fiber :: IORef Int -> Coroutine (Yield ()) IO ()
+fiber i = do
+            t <- lift (readIORef i)
+            yield ()
+            lift (writeIORef i (t + 1))
+            return ()
+
+main = do
+  i <- newIORef 0
+  let a = fiber i
+      b = fiber i in do
+      readIORef i >>= print -- 0
+      Left (Yield _ a') <- resume a
+      Left (Yield _ b') <- resume b
+      resume a'
+      resume b'
+      readIORef i >>= print -- should be 2
