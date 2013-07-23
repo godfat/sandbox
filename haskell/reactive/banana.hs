39795b267b74f632bef42d4854da751a6e8c3286
Lin Jen-Shin <godfat@godfat.org>
2013-07-24 02:52:00 +0800

    banana.hs: taste of reactive-banana

---
+++ haskell/reactive/banana.hs
@@ -0,0 +1,48 @@
+
+module Main where
+
+import Control.Monad (forever)
+
+-- cabal install ansi-terminal
+import System.Console.ANSI (setCursorPosition, clearScreen)
+
+-- cabal install reactive-banana
+import Reactive.Banana (Event, Moment, stepper)
+import Reactive.Banana.Frameworks
+
+main :: IO ()
+main = do
+  clearScreen
+  help
+  (handler, dispatch) <- newAddHandler
+  compile (fromAddHandler handler >>= setup) >>= actuate
+  forever (getLine >>= dispatch . read)
+
+
+help :: IO ()
+help = clearScreen >> setCursorPosition 0 0 >> putStrLn "Input a number:"
+
+
+setup :: Frameworks t => Event t Int -> Moment t ()
+setup eInput = do
+  eEven <- changes $ stepper True $ fmap even eInput
+  eOdd  <- changes $ stepper True $ fmap odd  eInput
+  register eInput eEven eOdd
+
+
+register :: Frameworks t =>
+            Event t Int -> Event t Bool -> Event t Bool -> Moment t ()
+register eInput eEven eOdd = do
+  reactimate $ fmap (const help) eInput
+
+  reactimate $ fmap (\n -> do
+                      setCursorPosition 1 0
+                      putStrLn ("Even? " ++ show n))
+                    eEven
+
+  reactimate $ fmap (\n -> do
+                      setCursorPosition 2 0
+                      putStrLn (" Odd? " ++ show n))
+                    eOdd
+
+  reactimate $ fmap (const clearScreen) eInput
