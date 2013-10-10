9d06b1738ba4e0438df99011d4772e0655d319ad
Lin Jen-Shin <godfat@godfat.org>
2013-10-10 11:39:28 +0800

    assemble with clang instead

--- c/llvm/build.sh
+++ c/llvm/build.sh
@@ -5,7 +5,7 @@ lli main.ll
 llvm-as main.ll -o main.bc
 lli main.bc
 llc main.bc -o main.s
-as main.s -o main.o
+clang -c main.s -o main.o
 ld /usr/lib/libc.dylib /usr/lib/crt1.o main.o -o main
 # ld -dynamic-linker /lib/ld-linux-x86-64.so.2 /usr/lib/libc.so /usr/lib/crt1.o /usr/lib/crti.o /usr/lib/crtn.o /usr/lib/libc_nonshared.a main.o -o main
 ./main
