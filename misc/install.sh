2d6cd5de6b0af09cedd4c5a6f95f8a383268ec58
Lin Jen-Shin <godfat@godfat.org>
2018-02-08 23:02:44 +0800

    Update the script to run rib for GitLab installation

--- misc/install.sh
+++ misc/install.sh
@@ -1,13 +1,9 @@
-# Ubuntu packages
-sudo apt install -y build-essential cmake libicu-dev pkg-config libpq-dev ruby-dev libreadline-dev nodejs libkrb5-dev
-
 # ENV
 GEM=~/.gem/ruby/2.3.0
 OMNIBUS=/opt/gitlab/embedded
-ROOT=$OMNIBUS/service/gitlab-rails
 
 # Extra gems
-$OMNIBUS/bin/gem install rib bond --user --no-ri --no-rdoc
+$OMNIBUS/bin/gem install rib bond readline_buffer --user --no-ri --no-rdoc
 
 # Run the console
-sudo $OMNIBUS/bin/chpst -e /opt/gitlab/etc/gitlab-rails/env -U `whoami` env PATH=$GEM/bin:$OMNIBUS/bin:$PATH GEM_PATH=$GEM:$OMNIBUS/lib/ruby/gems rib all -rrib/extra/paging -p $ROOT auto production
+sudo $OMNIBUS/bin/chpst -e /opt/gitlab/etc/gitlab-rails/env -U `whoami` env PATH=$GEM/bin:$OMNIBUS/bin:$PATH GEM_PATH=$GEM:$OMNIBUS/lib/ruby/gems EXECJS_RUNTIME=Disabled $OMNIBUS/bin/ruby -S $GEM/bin/rib all -rrib/extra/autoindent -rrib/extra/paging -p $OMNIBUS/service/gitlab-rails auto production
